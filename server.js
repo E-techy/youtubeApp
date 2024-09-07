3ce4891e5933341ee981dffaa8063c4ddee6f000
Ashutosh kumar Singh  /master <93425340+E-techy@users.noreply.github.com>
2024-09-07 23:59:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -175,6 +175,6 @@ app.post("/search",(req,res)=>{
 
 
 
-app.listen(3000, function () {
-  console.log("Listening on port 3000!");
+app.listen(443, function () {
+  console.log("Listening on port 443!");
 });
